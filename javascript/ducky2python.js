c309e3696b3d6d17cafcc0de16bdb86e8b47d3ac
CedArctic <kdoumani98@gmail.com>
2020-04-19 22:23:29 +0300

    Partial rewrite with many bug fixes
    - Fixed bugs mainly related to REPEAT and default DELAY
    - Re-wrote the command conversion to use a dictionary instead of parallel arrays
    - Added f* keys, numbers and special characters to hotkeys

--- javascript/ducky2python.js
+++ javascript/ducky2python.js
@@ -1,5 +1,3 @@
-// Inject bellow notes and ASCII art as default value of output element in web app
-
 /*	 
 
  duck2python converts DuckyScript scripts for the USB Rubber Ducky by hak5 to python scripts that function the same way
@@ -40,27 +38,31 @@ function convert(){
 	if (duckyScript[0].slice(0,7) == "DEFAULT"){
 		// Divide by 1000 because the time.sleep command takes seconds as an argument, not ms
 		defaultDelay = parseInt(duckyScript[0].slice(7)) / 1000;
+		duckyScript.shift();
 	}
 
 
 	// Variables:
 	var previousStatement = "";	
+	var keys = [];
 		
-	// Parallel command lists: 
-	// The first list contains the ducky commands and the second contains the corresponding pyautogui commands
-	var duckyCommands = ["WINDOWS", "GUI", "APP", "MENU", "SHIFT", "ALT", "CONTROL", "CTRL", "DOWNARROW", "DOWN",
-	"LEFTARROW", "LEFT", "RIGHTARROW", "RIGHT", "UPARROW", "UP", "BREAK", "PAUSE", "CAPSLOCK", "DELETE", "END",
-	"ESC", "ESCAPE", "HOME", "INSERT", "NUMLOCK", "PAGEUP", "PAGEDOWN", "PRINTSCREEN", "SCROLLLOCK", "SPACE", 
-	"TAB", "ENTER", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-	"u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
-	"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-	var pyautoguiCommands = ["win", "win", "optionleft", "optionleft", "shift", "alt", "ctrl", "ctrl", "down", "down",
-	"left", "left", "right", "right", "up", "up", "pause", "pause", "capslock", "delete", "end",
-	"esc", "escape", "home", "insert", "numlock", "pageup", "pagedown", "printscreen", "scrolllock", "space",
-	"tab", "enter", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-	"u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-	"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	// Dictionary containing Duckyscript and their corresponding pyautogui keys
+	var duckyCommands = {"WINDOWS":"win", "GUI":"win", "APP":"optionleft", "MENU":"optionleft",	"SHIFT":"shift",
+	"ALT":"alt", "CONTROL":"ctrl", "CTRL":"ctrl",	"DOWNARROW":"down", "DOWN":"down", "LEFTARROW":"left",
+	"LEFT":"left", "RIGHTARROW":"right", "RIGHT":"right", "UPARROW":"up",	"UP":"up", "BREAK":"pause",
+	"PAUSE":"pause", "CAPSLOCK":"capslock", "DELETE":"delete", "END":"end", "ESC":"esc", "ESCAPE":"esc",
+	"HOME":"home", "INSERT":"insert", "NUMLOCK":"numlock", "PAGEUP":"pageup", "PAGEDOWN":"pagedown",
+	"PRINTSCREEN":"printscreen", "SCROLLLOCK":"scrolllock", "SPACE":"space", "TAB":"tab", "ENTER":"enter",
+	"F1":"f1", "F2":"f2", "F3":"f3", "F4":"f4", "F5":"f5", "F6":"f6", "F7":"f7", "F8":"f8", "F9":"f9",
+	"F10":"f10", "F11":"f11", "F12":"f12", "a":"A", "b":"B", "c":"C", "d":"D", "e":"E", "f":"F", "g":"G",
+	"h":"H", "i":"I", "j":"J", "k":"K", "l":"L", "m":"M", "n":"N", "o":"O", "p":"P", "q":"Q", "r":"R",
+	"s":"S", "t":"T", "u":"U", "v":"V", "w":"W", "x":"X", "y":"Y", "z":"Z", "A":"A", "B":"B", "C":"C", "D":"D",
+	"E":"E", "F":"F", "G":"G", "H":"H", "I":"I", "J":"J", "K":"K", "L":"L", "M":"M", "N":"N", "O":"O", "P":"P",
+	"Q":"Q", "R":"R", "S":"S", "T":"T", "U":"U", "V":"V", "W":"W", "X":"X", "Y":"Y", "Z":"Z", "1":"1", "2":"2",
+	"3":"3", "4":"4", "5":"5", "6":"6", "7":"7", "8":"8", "9":"9", "0":"0", "!":"!", "\"":"\"", "#":"#", "$":"$",
+	"%":"%", "&":"&", "\'":"\'", "(":"(", ")":")", "*":"*", "+":"+", ",":",", "-":"-", ".":".", "/":"/", ":":":",
+	";":";", "<":"<", "=":"=", ">":">", "?":"?", "@":"@", "[":"[", "]":"]", "^":"^", "_":"_", "`":"`", "{":"{",
+	"|":"|", "}":"}", "~":"~"};
 
 	// Process each line from the Ducky Script:
 	for (line = 0; line < duckyScript.length; line++){
@@ -72,25 +74,29 @@ function convert(){
 			previousStatement = "time.sleep(" + (parseFloat(duckyScript[line].slice(6)) / 1000) + ")";
 		}else if (duckyScript[line].slice(0,6) == "STRING") {
 			previousStatement = "pyautogui.typewrite(\"" + duckyScript[line].slice(7) + "\", interval=0.02)";
-		}else if (duckyScript[line].slice(0,6) == "STRING") {
-			previousStatement = "pyautogui.typewrite(\"" + duckyScript[line].slice(7) + "\", interval=0.02)";
 		}else if (duckyScript[line].slice(0,6) == "REPEAT"){
-			for (i = 0; i < (parseInt(duckyScript[line].slice(7)) - 1); i++){
+			var repetitions = parseInt(duckyScript[line].slice(7)) - 1;
+			for (i = 0; i < repetitions; i++){
 				pythonScript += previousStatement;
 				pythonScript += "\n";
 
 				// Write Default Delay between the commands if it exists:
 				if (defaultDelay != 0){
-					pythonScript = "time.sleep(" + defaultDelay + ")" + "\n";
+					pythonScript += "time.sleep(" + defaultDelay + ")\n";
 				}
 			}
 		}else{
 			// Write beginning of command:
 			previousStatement = "pyautogui.hotkey(";
-			// Go through the parallel array and check if the examined key is in the command
-			for (j = 0; j < pyautoguiCommands.length; j++){
-				if (new RegExp("\\b"+duckyCommands[j]+"\\b").test(duckyScript[line])) {
-					previousStatement = previousStatement + "\'" + pyautoguiCommands[j] + "\',"; 
+			// Split statement into keys
+			keys = duckyScript[line].split(" ");
+			// Go through the keys matching them through the dictionary to pyautogui keys
+			for (j = 0; j < keys.length; j++){
+				if (keys[j] in duckyCommands){
+					previousStatement += "\"" + duckyCommands[keys[j]] + "\",";
+				}else{
+					// If it is not in the dictionary
+					previousStatement += "UNDEFINED_KEY" + ",";
 				}
 			}
 			// Remove last comma and add a parenthesis
@@ -103,7 +109,7 @@ function convert(){
 
 		// Write Default Delay if it exists:
 		if (defaultDelay != 0){
-			pythonScript = "time.sleep(" + defaultDelay + ")" + "\n";
+			pythonScript += "time.sleep(" + defaultDelay + ")" + "\n";
 		}
 	}
 	// Write Output
